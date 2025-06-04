8061d26112d2b41b8459e0153def4e17423db610
Thivaharan Adittan <t.adittan@gmail.com>
2025-06-04 21:11:36 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -27,7 +27,8 @@ const Dashboard = () => {
     return () => clearInterval(interval);
   }, []);
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://accounts-backend-production.up.railway.app";
+  //process.env.REACT_APP_API_URL
 
 axios.get(`${API_URL}/expenses`)
   .then(res => console.log(res.data))
