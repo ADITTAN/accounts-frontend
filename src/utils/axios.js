d81fe6e3169f6a3a7effb463be42b061fddbb096
Thivaharan Adittan <t.adittan@gmail.com>
2025-06-04 22:09:26 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance
 const instance = axios.create({
-  baseURL: 'http://localhost:7000/api', // Your backend base URL
+  baseURL: 'http://localhost:8080/api', // Your backend base URL
   headers: {
     'Content-Type': 'application/json',
   },
