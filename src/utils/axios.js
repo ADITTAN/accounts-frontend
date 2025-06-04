c451a9858a7f38fba46a6b8773dbcac047769832
Thivaharan Adittan <t.adittan@gmail.com>
2025-06-04 23:52:58 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance
 const instance = axios.create({
-  baseURL: 'https://accounts-backend-production.up.railway.app/api', // Your backend base URL
+  baseURL: 'https://accounts-frontend-hcpzp2dyn-adittans-projects.vercel.app/expenses/create', // Your backend base URL
   headers: {
     'Content-Type': 'application/json',
   },
